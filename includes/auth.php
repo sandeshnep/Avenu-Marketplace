c0c560cdce80cc215cc83b3706ae0ca0f0f95aae
lydiaphelps14 <32887163+lydiaphelps14@users.noreply.github.com>
2017-10-26 20:01:16 -0400

    Added cookie

--- includes/auth.php
+++ includes/auth.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+
+$cookie_name = "user";
+$cookie_value = $_SESSION['username'];
+setcookie($cookie_name, $cookie_value, time()+86400); //expires in one day
+
 if(!isset($_SESSION['username'])) {
     header("Location: ../login.php");
 }
