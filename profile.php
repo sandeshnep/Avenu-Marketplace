aef15abd055d5b42fd62c7434339c87c3be5dac0
Katastrophes <teronrussell23@gmail.com>
2017-10-28 16:49:42 -0400

    Fixed Merge Issue?

--- profile.php
+++ profile.php
@@ -10,12 +10,9 @@ include("includes/auth.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="css/profile.css" rel="stylesheet">
-<<<<<<< HEAD
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-=======
     <link rel="stylesheet" href="css/bootstrap.min.css">
->>>>>>> 47d3389f5e849734e0af1a053d24c2335ff78144
 </head>
 
 <body>
