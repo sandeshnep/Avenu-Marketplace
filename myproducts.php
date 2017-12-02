90f35ea371ecb790433a2671dcbeae77e260f323
Sandesh Paudel <spaudel@u.rochester.edu>
2017-12-02 15:25:03 -0500

    refined myproducts

--- myproducts.php
+++ myproducts.php
@@ -83,66 +83,13 @@ require_once('includes/header.php');
                 $row3 = mysqli_fetch_assoc($result3);
 
 
-                //runs if the UPDATE RATING button is pressed, updates the rating from the logged in user
-                if(isset($_POST['rating'])){
-
-                    $idreview = $_POST['rating'];
-                    $ratingval = $_POST['ratingval'];
-
-
-                    $search = "SELECT `comments` FROM `reviews` WHERE productid='$idreview'";
-
-                    $searchresult= mysqli_query($connect, $search) or die(mysqli_error());
-                    $searchrows = mysqli_num_rows($searchresult);
-
-                    //if an entry of comment exists, UPDATE the rating
-                    if ($searchrows>0){
-
-
-                        $query7 = "UPDATE `reviews` SET rating = '$ratingval' WHERE productid='$idreview' AND authorid='$username'";
-
-                        $result7=mysqli_query($connect, $query7);
-                        refresh();
-
-                    }
-                    else {
-
-                    //INSERT rating into the database
-                    $query4 = "REPLACE INTO `reviews` (`productid`, `rating`, `authorid`) VALUES ('$idreview', '$ratingval' , '$username')";
-                    $result4=mysqli_query($connect, $query4);
-                    refresh();
-
-                    }
-
-                }
-
-
 
                 //--------END OF TRANSFER OVER TO COMMON MARKETPLACE------------------------------------------------
 
 
 
        	    	echo '<li> <b>Productid : </b> ' . $row['productid'] . '<br><b>Product name: </b>' . $row['name']  . 
-                '<br><b>Product Description: </b>' . $row['description']  . '<br><b>Date Posted: </b>' . $row['timesql'] . '<br><b>Average Rating: ' . $row3['avg_rating'] . '</b>' .'<br><b>Your Rating:
-
-
-                <!-- UPDATE RATINGS - MOVE TO COMMON MARKET!--> 
-                <form method="POST">
-                <select name = "ratingval">
-                <option type ="number" value = "1">1</option>
-                <option type ="number" value = "2">2</option>
-                <option type ="number" value = "3">3</option>
-                <option type ="number" value = "4">4</option>
-                <option type ="number" value = "5">5</option>
-                </select>
-
-                </b>' .
-                '
-
-                <button type="submit" name = "rating" method ="POST" value = "' . $row['productid'] .'"class="btn btn-success">Update Rating</button>
-                </form>
-
-                <!-- UPDATE RATINGS - MOVE TO COMMON MARKET END!-->
+                '<br><b>Product Description: </b>' . $row['description']  . '<br><b>Date Posted: </b>' . $row['timesql'] . '<br><b>Average Rating: ' . $row3['avg_rating'] . '</b>
 
                 </li>
 
@@ -165,59 +112,8 @@ require_once('includes/header.php');
                  }
 
 
-                 if(isset($_POST['postcomment'])){
-
-                    $idcomment = $_POST['postcomment'];
-                    $commentinput = $_POST['commentinput'];
-
-                    $search2 = "SELECT 'rating' FROM `reviews` WHERE productid='$idcomment'";
-
-                    $searchresult= mysqli_query($connect, $search2) or die(mysqli_error());
-                    $searchrows = mysqli_num_rows($searchresult);
-
-                    //if an entry of rating exists, UPDATE comment
-                    if ($searchrows>0){
-
-                        $query8 = "UPDATE `reviews` SET comments = '$commentinput' WHERE productid='$idcomment' AND authorid='$username'";
-                        $result8=mysqli_query($connect, $query8);
-                        refresh();
-
-                    }
-                    else{
-
-                    //INSERT the rating into the database
-                    $query6 = "REPLACE INTO `reviews` (`productid`, `comments`, `authorid`) VALUES ('$idcomment', '$commentinput' , '$username')";
-                    $result6=mysqli_query($connect, $query6);
-                    refresh();
-                }
-
-
-
-                }
-
-
+                 
                  echo '
-
-
-                 <!-- form for adding a comment !-->
-
-                 <br> <br> <b> Add Comment: </b>
-
-                 <form name = "comment" method = "POST">
-                     <input name="commentinput" type = "text">
-                     <br>
-                     <br>
-
-                     <button type="submit" name="postcomment" value = "' . $row['productid'].'" class="btn btn-success"> Post Comment
-                     </button>
-
-                 </form>
-
-                 <br>
-
-
-
-
                 <!-- form buttons for DELETE !-->
 
                 <form name = "delete" method = "POST">
