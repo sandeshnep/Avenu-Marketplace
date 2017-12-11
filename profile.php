39b0feb5fec290459ab70535a1389201c374398f
Katastrophes <teronrussell23@gmail.com>
2017-12-11 16:28:54 -0500

    Added a Modal for Delete Account
    I felt it was necessary that there was a popup warning the user that they are deleting their account.

--- profile.php
+++ profile.php
@@ -96,9 +96,31 @@ require_once('includes/header.php');
 ?>
 
     <div class="container">
-        <form method="POST">
-            <button type="submit" name="delete" class="btn btn-danger btn-sm float-left">Delete Account</button>
-        </form>
+        
+            <button type="button" class="btn btn-danger btn-sm float-left" data-toggle="modal" data-target="#deleteModal">Delete Account</button>
+
+            <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">There is no turning back!</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            Are you sure you want to delete your account?
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                            <form method="POST">
+                            <button type="submit"  name="deleteyes"  class="btn btn-danger">Delete Account</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                    
 
     <br>
     <br>
